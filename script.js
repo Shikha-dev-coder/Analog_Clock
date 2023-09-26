ec4a3e9776f0aa36c86e2c914f244270125c7e52
Shikha-dev-coder <131350942+Shikha-dev-coder@users.noreply.github.com>
2023-09-26 16:40:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// for hour
+// 12 hour = 360deg
+// 1 hour = 360/12 = 30deg
+// h hour = 30h+ m/2
+
+// 60 min = 30
+// 1 min = 30/60 = 1/2
+// m min = (1/2)m
+
+// for minute
+// 60 min = 360 deg
+// 1 min = 360/60 = 6deg
+// m min = 6m
+
+// for second
+// 60 sec = 360 deg
+// 1 sec = 360/60 = 6 deg
+// s secs = 6s
+
+let hr = document.getElementById('hour');
+let min = document.getElementById('min');
+let sec = document.getElementById('sec');
+
+function displayTime(){
+    let date = new Date();
+    
+    //Getting hour, mins, secs from date
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    
+    let hRotation = 30*hh + mm/2;
+    let mRotation = 6*mm;
+    let sRotation = 6*ss;
+
+    hr.style.transform = `rotate(${hRotation}deg)`;
+    min.style.transform = `rotate(${mRotation}deg)`;
+    sec.style.transform = `rotate(${sRotation}deg)`;
+}
+
+setInterval(displayTime, 1000);
